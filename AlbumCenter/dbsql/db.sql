71cb8d1c13adcdb8c4e8b5d73c69f87c7faa753a
shaoguangyu <shaoguangyu@jd.com>
2015-10-24 17:35:26 +0800

    sql脚本

--- AlbumCenter/dbsql/db.sql
+++ AlbumCenter/dbsql/db.sql
@@ -1,10 +1,26 @@
 CREATE TABLE
-    t_album
+    t_album_basic_info
     (
         id bigint unsigned NOT NULL AUTO_INCREMENT,
-        name VARCHAR(100),
+        name VARCHAR(100) NOT NULL,
+        free CHAR(1) NOT NULL,
+        price TINYINT(3) unsigned,
+        remark VARCHAR(1000),
         cover_image_url VARCHAR(100),
-        inside_front_cover_image_url VARCHAR(100),
+        cover_image2_url VARCHAR(100),
+        PRIMARY KEY (id)
+    )
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
+CREATE TABLE
+    t_image
+    (
+        id bigint NOT NULL AUTO_INCREMENT,
+        file_name VARCHAR(1000) NOT NULL,
+        file_size bigint NOT NULL,
+        url VARCHAR(100) NOT NULL,
+        create_time DATETIME NOT NULL,
+        status VARCHAR(5) NOT NULL,
         PRIMARY KEY (id)
     )
     ENGINE=InnoDB DEFAULT CHARSET=utf8;
