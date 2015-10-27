81f0d03f82f53f9b9b864f6683e116eba01761ec
shaoguangyu <shaoguangyu@jd.com>
2015-10-27 09:50:32 +0800

    参数问题

--- AlbumCenter/src/main/java/com/lusoft/album/controller/CommonController.java
+++ AlbumCenter/src/main/java/com/lusoft/album/controller/CommonController.java
@@ -1,9 +1,11 @@
 package com.lusoft.album.controller;
 
-import java.util.Map;
+import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,19 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(path="/common")
 public class CommonController {
 
+	private Logger logger = LogManager.getLogger(CommonController.class);
+	
 	@RequestMapping(path="/go/{name}")
 	public ModelAndView gotoJsp(@PathVariable String name, HttpServletRequest request){
 		String jsp = name.replaceAll("!", "/");
 		ModelAndView view = new ModelAndView(jsp);
-		Map<String, Object> param = request.getParameterMap();
-		view.addAllObjects(param);
+		Enumeration<String> e = request.getParameterNames();
+		while(e.hasMoreElements()) {
+			String key = e.nextElement();
+			String value = request.getParameter(key);
+			logger.debug("key->" + key + ", value->" + value);
+			view.addObject(key, value);
+		}
 		return view;
 	}
 }
